394c23528ec51eb18b539c0abe279d49f928d840
Codecana13 <arthurcodec@gmail.com>
2025-04-21 21:24:34 +0700

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 text-center p-4 mt-10 border-t border-gray-700">
-      <p>© 2024 DataPembayaran | All Rights Reserved by RegWell.</p>
+      <p>© 2025 DataPembayaran | All Rights Reserved.</p>
     </footer>
   );
 }
